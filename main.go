a80c26526f50254eee75878e4bbf820727d12ba7
Michael Ethridge <methridge@hashicorp.com>
2021-06-23 11:08:44 -0600

    Logging and Health updates
    Signed-off-by: Michael Ethridge <methridge@hashicorp.com>

--- main.go
+++ main.go
@@ -1,89 +1,38 @@
 package main
 
 import (
+	"context"
 	"embed"
+	"flag"
 	"fmt"
 	"io/fs"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync/atomic"
 	"text/template"
+	"time"
 
 	"cloud.google.com/go/compute/metadata"
 )
-
-type gcpInstance struct {
-	Attributes struct {
-	} `json:"attributes"`
-	CPUPlatform string `json:"cpuPlatform"`
-	Description string `json:"description"`
-	Disks       []struct {
-		DeviceName string `json:"deviceName"`
-		Index      int    `json:"index"`
-		Interface  string `json:"interface"`
-		Mode       string `json:"mode"`
-		Type       string `json:"type"`
-	} `json:"disks"`
-	GuestAttributes struct {
-	} `json:"guestAttributes"`
-	Hostname             string `json:"hostname"`
-	ID                   int64  `json:"id"`
-	Image                string `json:"image"`
-	LegacyEndpointAccess struct {
-		Zero1   int `json:"0.1"`
-		V1Beta1 int `json:"v1beta1"`
-	} `json:"legacyEndpointAccess"`
-	Licenses []struct {
-		ID string `json:"id"`
-	} `json:"licenses"`
-	MachineType       string `json:"machineType"`
-	MaintenanceEvent  string `json:"maintenanceEvent"`
-	Name              string `json:"name"`
-	NetworkInterfaces []struct {
-		AccessConfigs []struct {
-			ExternalIP string `json:"externalIp"`
-			Type       string `json:"type"`
-		} `json:"accessConfigs"`
-		DNSServers        []string      `json:"dnsServers"`
-		ForwardedIps      []interface{} `json:"forwardedIps"`
-		Gateway           string        `json:"gateway"`
-		IP                string        `json:"ip"`
-		IPAliases         []interface{} `json:"ipAliases"`
-		Mac               string        `json:"mac"`
-		Mtu               int           `json:"mtu"`
-		Network           string        `json:"network"`
-		Subnetmask        string        `json:"subnetmask"`
-		TargetInstanceIps []interface{} `json:"targetInstanceIps"`
-	} `json:"networkInterfaces"`
-	Preempted        string `json:"preempted"`
-	RemainingCPUTime int    `json:"remainingCpuTime"`
-	Scheduling       struct {
-		AutomaticRestart  string `json:"automaticRestart"`
-		OnHostMaintenance string `json:"onHostMaintenance"`
-		Preemptible       string `json:"preemptible"`
-	} `json:"scheduling"`
-	ServiceAccounts struct {
-		Three26640127649ComputeDeveloperGserviceaccountCom struct {
-			Aliases []string `json:"aliases"`
-			Email   string   `json:"email"`
-			Scopes  []string `json:"scopes"`
-		} `json:"326640127649-compute@developer.gserviceaccount.com"`
-		Default struct {
-			Aliases []string `json:"aliases"`
-			Email   string   `json:"email"`
-			Scopes  []string `json:"scopes"`
-		} `json:"default"`
-	} `json:"serviceAccounts"`
-	Tags         []string `json:"tags"`
-	VirtualClock struct {
-		DriftToken string `json:"driftToken"`
-	} `json:"virtualClock"`
-	Zone string `json:"zone"`
-}
 
 //Index holds fields displayed on the index.html template
 type Index struct {
 	Hostname string
 	Tags     []string
 }
+
+type key int
+
+const (
+	requestIDKey key = 0
+)
+
+var (
+	listenAddr string
+	healthy    int32
+)
 
 //go:embed static
 var staticFiles embed.FS
@@ -96,16 +45,70 @@ var errFile string
 
 // This example demonstrates how to use your own transport when using this package.
 func main() {
+	flag.StringVar(&listenAddr, "listen-addr", ":80", "server listen address")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "gcp-meta: ", log.LstdFlags)
+	logger.Println("GCP Metadata reader is starting...")
 
 	fsys, err := fs.Sub(staticFiles, "static")
 	if err != nil {
 		fmt.Print(err.Error())
 	}
-	http.Handle("/static/",
+
+	router := http.NewServeMux()
+	router.Handle("/", index())
+	router.Handle("/healthz", healthz())
+	router.Handle("/static/",
 		http.StripPrefix("/static/",
 			http.FileServer(http.FS(fsys))))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	nextRequestID := func() string {
+		return fmt.Sprintf("%d", time.Now().UnixNano())
+	}
+
+	server := &http.Server{
+		Addr:         listenAddr,
+		Handler:      tracing(nextRequestID)(logging(logger)(router)),
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	done := make(chan bool)
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		<-quit
+		logger.Println("GCP Metadata reader is shutting down...")
+		atomic.StoreInt32(&healthy, 0)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		server.SetKeepAlivesEnabled(false)
+		if err := server.Shutdown(ctx); err != nil {
+			logger.Fatalf("Could not gracefully shutdown the server: %v\n", err)
+		}
+		close(done)
+	}()
+
+	logger.Println("GCP Metadata reader server is ready to handle requests at", listenAddr)
+	atomic.StoreInt32(&healthy, 1)
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatalf("Could not listen on %s: %v\n", listenAddr, err)
+	}
+
+	<-done
+	logger.Panicln("GCP Metadata reader stopped")
+	// fmt.Println(http.ListenAndServe(":80", nil))
+}
+
+func index() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
 		if metadata.OnGCE() {
 			h, err := metadata.Hostname()
 			if err != nil {
@@ -133,5 +136,43 @@ func main() {
 			}
 		}
 	})
-	fmt.Println(http.ListenAndServe(":80", nil))
+}
+
+func healthz() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if atomic.LoadInt32(&healthy) == 1 {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		w.WriteHeader(http.StatusServiceUnavailable)
+	})
+}
+
+func logging(logger *log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				requestID, ok := r.Context().Value(requestIDKey).(string)
+				if !ok {
+					requestID = "unknown"
+				}
+				logger.Println(requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
+			}()
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func tracing(nextRequestID func() string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			requestID := r.Header.Get("X-Request-Id")
+			if requestID == "" {
+				requestID = nextRequestID()
+			}
+			ctx := context.WithValue(r.Context(), requestIDKey, requestID)
+			w.Header().Set("X-Request-Id", requestID)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
 }
